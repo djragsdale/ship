269ce8316d3c66a42a43f27f6ce917d1b92bc5f7
David Ragsdale <me@davidjragsdale.com>
2020-01-11 02:43:13 -0500

    Scripts evaluate

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const evaluate = require('./lib/evaluate');
 const execute = require('./lib/execute');
-const keywords = require('./lib/keywords');
+const keywords = require('./lib/constants/keywords');
+const operators = require('./lib/constants/operators');
 const Parser = require('./lib/Parser');
 
 module.exports = {
   evaluate,
   execute,
   keywords,
+  operators,
   Parser,
 };
